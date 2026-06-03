2a10cd0f0e893ef95f16809bcfb6b5e3ce43919d
SerenK00 <serenkaya2004@gmail.com>
2026-06-03 13:09:19 +0300

    add controllers

---
+++ src/main/java/bm/erciyes/robotvacuumsim/Launcher.java
@@ -0,0 +1,27 @@
+package bm.erciyes.robotvacuumsim;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Launcher extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                Launcher.class.getResource("main-view.fxml")
+        );
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Robot Süpürge Simülasyonu");
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
